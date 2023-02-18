8818a3905d1f4a64764527c4ef2c4e3e1f5f4970
RyanPendergast <58488585+RyanPendergast@users.noreply.github.com>
2023-02-17 21:42:45 -0600

    Create Firewalld.sh

---
+++ LinuxSystems/Firewalld.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+#This script sets up CentOD Firewalld Service
+#Firewalld is included by default  
+
+#start firewall service and enable on boot
+systemctl start firewalld
+systemctl enable firewalld
+
+#Check firewall state
+firewall-cmd --state
+
+#Check firewall status
+systemctl status firewalld
+
+chmod +x firewalld.sh
+sudo ./firewalld.sh
